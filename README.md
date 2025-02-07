14c44be177504d46a5a4b297d33e2c5443efe8eb
Adithyan0896 <sriadibav96@gmail.com>
2025-02-07 20:41:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+## Student Management System:
+
+
+### Description:
+
+This project involves designing a **relational database schema** for a **Student Management System **. The database includes essential tables to manage student information, courses, enrollments, grades, instructors, and course-instructor assignments. It supports functionalities for handling student registrations, course enrollments, grading, and instructor assignments efficiently.
+
+
+### Features:
+
+- **Student Management**: Handles storing and retrieving student details such as names, emails, contact information, and addresses.
+  
+- **Course Management**: Manages course details, including course names, descriptions, and credits.
+
+- **Enrollment Management**: Tracks student enrollments in various courses and their respective enrollment dates.
+
+- **Grade Management**: Stores student grades and GPAs for completed courses.
+
+- **Instructor Management**: Stores instructor information, including contact details and department assignments.
+
+- **Course-Instructor Assignment**: Links courses with the instructors teaching them.
+
+
+### Database Schema:
+
+The database consists of the following tables:
+
+- **Students:** Stores details about the students, including student ID, name, contact details, and address.
+  
+- **Courses:** Contains information about the courses offered, including course ID, name, description, and credits.
+  
+- **Enrollments:** Links students to the courses they are enrolled in, with enrollment date.
+
+- **Grades:** Contains students' grades and GPAs for each course they are enrolled in.
+
+- **Instructors:** Stores information about instructors, including their ID, name, contact details, and department.
+
+- **Course Instructors:** Links instructors to the courses they teach.
+
+
+### Technologies Used:
+
+- **SQL (MySQL)**: Used for creating and managing the relational database schema.
+
+- **Relational Database Management System (RDBMS)**: A system to handle data integrity, relationships, and queries between different tables.
+
+
+### Queries and Analysis:
+
+The project includes various SQL queries and data analysis tasks to extract valuable insights from the student management data. Some of the key queries implemented include:
+
+**1. JOIN:**  
+   - Used to retrieve detailed information about students, their enrolled courses, grades, and instructors. For example, joining the `Students`, `Enrollments`, `Courses`, `Grades`, and `Instructors` tables to retrieve a list of students along with their grades, GPAs, and instructor names.
+
+    
+
+**2. VIEW:**  
+   - Created a `StudentTranscript` view to generate a summarized report of students’ enrolled courses along with their grades and GPAs.
+
+   
+**3. SUBQUERY:**  
+   - Used a subquery to find students who are enrolled in a specific course.
+
+   
+**4. STORED PROCEDURE:**  
+   - Created a stored procedure to retrieve student transcripts, including course names, grades, and GPA for each student.
+
---
+++ Student Management System.sql
@@ -0,0 +1,181 @@
+                                      -- ****** Student Management System ******
+-- Creating DATABASE (Name:SMS )
+create database StudentManagementSystem;
+use StudentManagementSystem;
+
+-- Students Table 
+CREATE TABLE Students (
+  StudentID INT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL,
+  Email VARCHAR(100) NOT NULL,
+  Phone VARCHAR(20) NOT NULL,
+  Address VARCHAR(200) NOT NULL
+);
+
+-- Insert data into students
+INSERT INTO Students (StudentID, Name, Email, Phone, Address)
+VALUES
+(1, 'Venkat', 'venkat01@gmail.com', '123-456-7890', '123 Main St,adyar,chennai'),
+(2, 'varadha', 'varadha02@gmail.com', '987-654-3210', '456 ramakrishna St,egmore,chennai'),
+(3, 'sri', 'sri03@gmail.com', '555-123-4567', '789 bharatiyar st ,ambattur,chennai'),
+(4, 'ram', 'ram04@gmail.com', '901-234-5678', '321 main St ,guindy,chennai'),
+(5, 'vivek', 'vivek05@gmail.com', '111-222-3333', '444 kamaraj St,mylapore,chennai');
+
+select * from students;
+
+
+-- Courses Table 
+CREATE TABLE Courses (
+  CourseID INT PRIMARY KEY,
+  CourseName VARCHAR(100) NOT NULL,
+  CourseDescription VARCHAR(200) NOT NULL,
+  Credits INT NOT NULL
+);
+
+-- Insert data into courses
+INSERT INTO Courses (CourseID, CourseName, CourseDescription, Credits)
+VALUES
+(1, 'data science 101', ' introduction to machine learning', 3),
+(2, 'full stack developement 102', 'Introduction to full stack ', 4),
+(3, 'data analytics 103', 'Introduction to data analytics', 3),
+(4, 'cloud computing 104', 'Introduction to cloud computing', 4),
+(5, 'devops 105', 'Introduction to devops', 3);
+
+select * from courses;
+
+-- Enrollments Table 
+CREATE TABLE Enrollments (
+  EnrollmentID INT PRIMARY KEY,
+  StudentID INT NOT NULL,
+  CourseID INT NOT NULL,
+  EnrollmentDate DATE NOT NULL,
+  FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+  FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
+
+
+-- Insert data into Enrollments
+INSERT INTO Enrollments (EnrollmentID, StudentID, CourseID, EnrollmentDate)
+VALUES
+(1, 1, 1, '2024-01-01'),
+(2, 1, 2, '2024-02-01'),
+(3, 2, 1, '2024-03-01'),
+(4, 2, 3, '2024-04-01'),
+(5, 3, 2, '2024-05-01'),
+(6, 3, 4, '2024-06-01'),
+(7, 4, 1, '2024-07-01'),
+(8, 4, 5, '2024-08-01'),
+(9, 5, 3, '2024-09-01'),
+(10, 5, 4, '2024-10-01');
+
+select * from enrollments;
+
+-- Grades Table 
+CREATE TABLE Grades (
+  GradeID INT PRIMARY KEY,
+  EnrollmentID INT NOT NULL,
+  Grade VARCHAR(2) NOT NULL,
+  GPA DECIMAL(3,2) NOT NULL,
+  FOREIGN KEY (EnrollmentID) REFERENCES Enrollments(EnrollmentID)
+);
+
+-- Insert data into Grades
+INSERT INTO Grades (GradeID, EnrollmentID, Grade, GPA)
+VALUES
+(1, 1, 'A', 9.0),
+(2, 2, 'B', 8.0),
+(3, 3, 'A', 9.0),
+(4, 4, 'B', 8.0),
+(5, 5, 'C', 7.0),
+(6, 6, 'A', 9.0),
+(7, 7, 'B', 8.0),
+(8, 8, 'C', 7.0),
+(9, 9, 'D', 6.0),
+(10, 10, 'F', 4.0);
+
+select * from grades;
+
+-- Instructors Table 
+CREATE TABLE Instructors (
+  InstructorID INT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL,
+  Email VARCHAR(100) NOT NULL,
+  Phone VARCHAR(20) NOT NULL,
+  Department VARCHAR(50) NOT NULL
+);
+
+-- Insert data into Instructors
+INSERT INTO Instructors (InstructorID, Name, Email, Phone, Department)
+VALUES
+(1, 'mr.shiva', 'shiva@gmail.com', '555-123-4567', 'information technology'),
+(2, 'mr.kumar', 'kumar@gmail.com', '901-234-5678', 'computer Science'),
+(3, 'mr.rajesh', 'rajesh@gmail.com', '111-222-3333', 'artificial intelligence'),
+(4, 'mrs.sheela', 'sheela@gmail.com', '444-555-6666', 'computer science'),
+(5, 'mrs.rajee', 'rajee@gmail.com', '777-888-9999', 'information technology');
+
+select * from instructors;
+
+-- courseInstructors Table 
+CREATE TABLE CourseInstructors (
+  CourseID INT NOT NULL,
+  InstructorID INT NOT NULL,
+  FOREIGN KEY (CourseID) REFERENCES Courses(CourseID),
+  FOREIGN KEY (InstructorID) REFERENCES Instructors(InstructorID)
+);
+
+-- Insert data into courseinstructors 
+INSERT INTO CourseInstructors (CourseID, InstructorID)
+VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
+
+select * from courseinstructors;
+
+
+-- Using JOINS
+SELECT s.StudentID, s.Name, c.CourseName, g.Grade, g.GPA, i.Name AS InstructorName
+FROM Students s
+INNER JOIN Enrollments e ON s.StudentID = e.StudentID
+INNER JOIN Courses c ON e.CourseID = c.CourseID
+INNER JOIN Grades g ON e.EnrollmentID = g.EnrollmentID
+INNER JOIN CourseInstructors ci ON c.CourseID = ci.CourseID
+INNER JOIN Instructors i ON ci.InstructorID = i.InstructorID
+WHERE c.CourseID = 1;
+
+-- View
+CREATE VIEW StudentTranscript AS
+SELECT s.StudentID, s.Name, c.CourseName, g.Grade, g.GPA
+FROM Students s
+JOIN Enrollments e ON s.StudentID = e.StudentID
+JOIN Courses c ON e.CourseID = c.CourseID
+JOIN Grades g ON e.EnrollmentID = g.EnrollmentID;
+select * from studenttranscript;
+
+-- SUBQUERY
+SELECT *
+FROM Students
+WHERE StudentID IN (
+SELECT StudentID
+FROM Enrollments
+WHERE CourseID = 1
+);                                       
+
+-- STORED PROCEDURE:
+
+DELIMITER $$
+
+CREATE PROCEDURE GetStudentTranscript()
+BEGIN
+    SELECT s.StudentID, s.Name, c.CourseName, g.Grade, g.GPA
+    FROM Students s
+    JOIN Enrollments e ON s.StudentID = e.StudentID
+    JOIN Courses c ON e.CourseID = c.CourseID
+    JOIN Grades g ON e.EnrollmentID = g.EnrollmentID;
+END $$
+
+DELIMITER ;
+
+call getstudenttranscript();
